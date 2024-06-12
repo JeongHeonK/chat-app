1bee5aecea2409d37040fa65d954e9c0dcac7db3
김정헌 <metronom7703@naver.com>
2024-06-12 17:23:09 +0900

    에러처리

--- app.js
+++ app.js
@@ -1,36 +1,35 @@
 import express from 'express';
 import { createServer } from 'node:http';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
 import { Server } from 'socket.io';
 import cors from 'cors';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
-const server = createServer(app);
-
-const io = new Server(server, {
-  cors: {
-    origin: '*',
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['Content-Type'],
-  },
-});
 
 app.use(cors());
-app.use(express.json());
+const server = createServer(app);
+const io = new Server(server);
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'index.html'));
+});
 
 io.on('connection', (socket) => {
-  console.log('WebSocket connected');
-
-  socket.on('message', (data) => {
-    io.emit('res', data);
-  });
-
+  console.log('a user connected');
   socket.on('disconnect', () => {
-    console.log('WebSocket disconnected');
+    console.log('user disconnected');
+  });
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
   });
 });
 
-const PORT = 8080;
-server.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+server.listen(process.env.PORT, () => {
+  console.log(`Server running!!`);
 });
-K;
